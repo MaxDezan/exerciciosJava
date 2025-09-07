c6aaa231788b66fc7fd9017f7c98efad648c9879
MaxDezan <maxdezan@rede.ulbra.br>
2025-09-07 14:14:14 -0300

    3

---
+++ src/divisao/Dividir.java
@@ -0,0 +1,24 @@
+package divisao;
+
+import java.util.Scanner;
+
+public class Dividir {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in).useLocale(java.util.Locale.US);
+
+        System.out.print("Digite o dividendo: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Digite o divisor: ");
+        double b = sc.nextDouble();
+
+        if (b == 0) {
+            System.out.println("Divisão por zero não permitida.");
+        } else {
+            double resultado = a / b;
+            System.out.println("Resultado: " + resultado);
+        }
+
+        sc.close();
+    }
+}
---
+++ src/soma/Soma.java
@@ -0,0 +1,26 @@
+package soma;
+
+import java.util.Scanner;
+
+public class Soma {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in).useLocale(java.util.Locale.US);
+        /*System.out.println("Digite dois valores para somar");
+        System.out.println("Primeiro valor: ");int a = sc.nextInt();
+        System.out.println("Segundo valor: ");int b = sc.nextInt();
+        System.out.println("A soma vale: " + (a + b));
+        sc.close();*/
+
+        System.out.print("Digite o primeiro número decimal: ");
+        double c = sc.nextDouble();
+
+        System.out.print("Digite o segundo número decimal: ");
+        double d = sc.nextDouble();
+
+        double soma = c + d;
+        System.out.println("A soma vale: " + soma);
+
+        sc.close();
+    }
+}
+
