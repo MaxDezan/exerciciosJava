e5290005c605fae5c016078daca81f57c0e15e65
MaxDezan <maxdezan@rede.ulbra.br>
2025-09-08 14:57:02 -0300

    5

---
+++ src/cofrinho/Cofre.java
@@ -0,0 +1,21 @@
+package cofrinho;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Cofre {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in).useLocale(java.util.Locale.US);
+
+        BigDecimal cofre = BigDecimal.ZERO;
+
+        System.out.println("Bem-vindo(a) ao seu cofrinho.");
+        System.out.print("Digite quanto deseja depositar: ");
+        BigDecimal depositar = sc.nextBigDecimal();
+        cofre = cofre.add(depositar);
+
+        System.out.println("Depósito concluído. O saldo atual é de R$" + cofre);
+
+        sc.close();
+    }
+}
